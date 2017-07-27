0595af601ee47b6112f977deca6535be16642351
frankli9 <casablanca_9999@sina.com>
2017-07-27 15:46:58 +0800

    Update word-len.py

--- word-len.py
+++ word-len.py
@@ -1,9 +1,9 @@
 
-file_object=open('E:\\Chinese\\普通话\\set_word.txt','rt',encoding='utf-8')#gb2312/gb18030，
+file_object=open('E:\\Chinese\\1\\set_word.txt','rt',encoding='utf-8')#gb2312/gb18030，
 lines_seen = set()
 for line in file_object:
     if len(line) >= 7 and len(line) <= 35 and line not in lines_seen:
-        text_1 = open('E:\\Chinese\\普通话\\整理\\字数限制7-35\\set_word-limit-7-35.txt', 'a',encoding='utf-8')
+        text_1 = open('E:\\Chinese\\1\\2\\3\\set_word-limit-7-35.txt', 'a',encoding='utf-8')
         text_1.write(line)
         lines_seen.add(line)
         print(line)
